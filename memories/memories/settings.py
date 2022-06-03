5d024f9f81b484374a27beb5e337f49f346c2bfd
godwinwabwile <wegowab1@gmail.com>
2022-06-03 07:27:24 +0300

    included all basic scarfold files

--- memories/memories/settings.py
+++ memories/memories/settings.py
@@ -123,4 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR= os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR =[
+    STATIC_DIR,
+    BASE_DIR/'posts'/'static',
+    BASE_DIR/'profiles'/'static',
+    ]
+
+MEDIA_URL="/media/"
+MEDIA_ROOT= BASE_DIR/'media'
+
--- memories/posts/views.py
+++ memories/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    topic= "Django"
+    description= "Django is an amazing web framework"
+
+    return render(request, "posts/main.html", {"topic": topic, "description":description})
